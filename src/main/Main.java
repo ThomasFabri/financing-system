0b7b421f328f59bb15a108d5a92350b766742f41
Thomas Fabri <tsfabri@gmail.com>
2025-04-18 10:39:40 -0300

    First-version
    The project is showing pre-made values and showing the total value at the start of the application.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,15 +3,30 @@ package main;
 import modelo.Financiamento;
 import util.InterfaceUsuario;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
         InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
 
-        double valorImovel = interfaceUsuario.pedirValorImovel();
-        int prazoFinanciamentoAnos = interfaceUsuario.pedirPrazoFinanciamento();
-        double taxaJuros = interfaceUsuario.pedirTaxaJuros();
+        ArrayList<Financiamento> financiamentos = new ArrayList<>();
 
-        Financiamento financiamento = new Financiamento(valorImovel, prazoFinanciamentoAnos, taxaJuros);
+        // Adicionando 4 financiamentos manualmente
+        financiamentos.add(new Financiamento(200000, 30, 0.04));
+        financiamentos.add(new Financiamento(300000, 20, 0.06));
+        financiamentos.add(new Financiamento(150000, 15, 0.03));
+        financiamentos.add(new Financiamento(250000, 25, 0.05));
 
+        double totalValorImoveis = 0;
+        double totalValorFinanciamentos = 0;
+
+        for (Financiamento f : financiamentos) {
+            totalValorImoveis += f.getValorImovel();
+            totalValorFinanciamentos += f.calcularTotalPagamento();
+        }
+
+        System.out.println("Total de todos os imóveis: R$ " + totalValorImoveis);
+        System.out.println("Total de todos os financiamentos: R$ " + totalValorFinanciamentos);
     }
 }
